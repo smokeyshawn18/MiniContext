861c28f362032a72e7ff87ab97bef5f09101f3bd
Shudar_shawn <shudarsanpoudel25@gmail.com>
2024-10-01 13:03:59 +0545

    ContextSimple

---
+++ src/context/UserContextProvider.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import UserContext from "./UserContext";
+
+const ContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default ContextProvider;
